a74e9063394dd145b37a88c58266ff430620c94b
Marouan El Yattafti <marouanely@gmail.com>
2022-07-02 14:35:04 +0200

    full app

---
+++ components/index.js
@@ -0,0 +1,20 @@
+import HomeHeader from './HomeHeader';
+import DetailsHeader from './DetailsHeader';
+import FocusedStatusBar from './FocusedStatusBar';
+import NFTCard from './NFTCard';
+import { CircleButton, RectButton } from './Button';
+import DetailsDesc from './DetailsDesc';
+import DetailsBid from './DetailsBid';
+import { SubInfo } from './SubInfo';
+
+export {
+  HomeHeader,
+  DetailsHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  DetailsDesc,
+  DetailsBid,
+  SubInfo,
+};
